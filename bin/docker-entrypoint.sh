1cb1699e20439bfff48ef667f6ac3fdb07582d40
Olivier Gintrand <olivier.gintrand@thalesgroup.com>
2018-02-20 13:24:32 +0100

    no template

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -4,8 +4,8 @@ echo "Generating cron schedules"
 echo "${BARMAN_CRON_SCHEDULE} root barman cron" > /etc/crontab
 echo "${BARMAN_BACKUP_SCHEDULE} root barman backup all" >> /etc/crontab
 
-echo "Generating Barman configurations"
-cat /etc/barman.conf.template | envsubst > /etc/barman.conf
+#echo "Generating Barman configurations"
+#cat /etc/barman.conf.template | envsubst > /etc/barman.conf
 
 echo "Checking/Creating replication slot"
 barman replication-status pg --minimal --target=wal-streamer | grep barman || barman receive-wal --create-slot pg
